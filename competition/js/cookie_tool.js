9840d2985fbb93e74317147ca623eeae3ecfa6d5
lbh117583010116 <1090184468@qq.com>
2020-06-14 10:28:56 +0800

    提交

---
+++ competition/js/cookie_tool.js
@@ -0,0 +1,19 @@
+function exit(){
+	
+	$.removeCookie("username");
+	$.removeCookie("TOKEN");
+	window.location.href="login.html"
+}
+$(".Left").load("left.html",function(){
+	if($.cookie("type")==0||$.cookie("type")==null){
+		$(".notauthority").remove(); 
+	} 
+});
+$(".Header").load("header.html",function(){
+	document.getElementById("backToIndex").onclick = function() {
+		window.location.href = "index.html";
+	}
+	document.getElementById("backToIndex").onmouseover = function() {
+		this.style.cursor = "pointer";
+	}
+})
